392eaa452c9c8487416a31ed18d1752d576383b6
Satish Kumar Chaudhary <skc05415518@gmail.com>
2025-01-18 21:21:11 +0545

    v2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //use express
 const express=require('express')
 const { blogs } = require('./model/index')
+const { where } = require('sequelize')
 const app=express()
 
 // import index file inside the model
@@ -15,6 +16,7 @@ app.use(express.urlencoded({extended:true}))
 
 // create api 
 app.get('/',async(req,res)=>{
+    // read all data from database
     const allBlog =await blogs.findAll()
     
     res.render('home', {blogs:allBlog})
@@ -23,6 +25,7 @@ app.get('/createblog',(req,res)=>{
     res.render('createblog')
 })
 
+// insert data into database
 app.post('/createblog',async(req,res)=>{
     const title = req.body.title
     const subtitle =req.body.subtitle
@@ -32,10 +35,38 @@ app.post('/createblog',async(req,res)=>{
         subtitle : subtitle,
         description : description
     })
-    res.send("data submitted successfully")
+    res.redirect('/')
+})
+
+// read single blog
+app.get('/read/:id',async(req,res)=>{
+    const id = req.params.id
+    const singleBlog =await blogs.findAll({
+        where: {
+          id : id  
+        }
+    })
+    res.render('viewsingleblog', {singleBlog:singleBlog})
+})
+
+// delete single blog
+app.get('/delete/:id',async(req,res)=>{
+    const id = req.params.id
+   await blogs.destroy({
+        where :{
+            id : id
+        }
+    })
+    res.redirect('/')
+})
+
+// edit blog
+app.get('/edit/:id',(req,res)=>{
+ console.log('edit')
+
 })
 
 // port to run project
-app.listen(3000,(req,res)=>{
+app.listen(3000,()=>{
     console.log('project will be ready on port no 3000')
 })
